af5b1961204acf5bbe08b04d9a4f7503691483d2
mpendulo-dev <mpendulo.dev@gmail.com>
2023-05-20 13:52:13 +0200

    progmatic navigation in swift: ios

--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -9,11 +9,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Global button
+    let button = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+        
+        button.setTitle("Go to Nav Controller", for: .normal)
+        view.addSubview(button)
+        button.backgroundColor = .white
+        button.setTitleColor(.black, for: .normal)
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 52)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+    }
+    
+    // function to navigate
+    
+    @objc private func didTapButton() {
+        let rootVC = SecondViewController()
+        let navVC = UINavigationController(rootViewController: rootVC)
+        navVC.modalPresentationStyle = .fullScreen
+        present(navVC, animated: true)
     }
 
+}
 
+//Second view with red background
+class SecondViewController: UIViewController {
+    private let button = UIButton()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemRed
+        title = "Welcome"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Dismiss", style: .plain, target: self, action: #selector(dismissSelf))
+        
+        button.setTitle("Go to Nav Controller", for: .normal)
+        view.addSubview(button)
+        button.backgroundColor = .white
+        button.setTitleColor(.black, for: .normal)
+        button.frame = CGRect(x: 100, y: 100, width: 200, height: 52)
+        button.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
+        
+        
+    }
+    
+    @objc private func didTapButton() {
+        let vc = UIViewController()
+        vc.view.backgroundColor = .white
+        navigationController?.pushViewController(vc, animated: true)
+    }
+    
+    @objc private func dismissSelf() {
+        dismiss(animated: true)
+    }
 }
 
